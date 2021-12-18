4123eb5ea447b24965a181d432dcab6009efcd5e
vicentefb <chenteferrara@gmail.com>
2021-12-17 18:04:29 -0600

    added section and card components to view movies by genre

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { gql, GraphQLClient } from "graphql-request";
+import Section from "../components/Section";
 
 export const getStaticProps = async () => {
   const url = process.env.ENDPOINT;
@@ -39,6 +40,34 @@ export const getStaticProps = async () => {
 };
 
 export default function Home({ videos }) {
-  console.log(videos);
-  return <div></div>;
+  const randomVideo = (videos) => {
+    return videos[Math.floor(Math.random() * videos.length)];
+  };
+
+  const filterVideos = (videos, genre) => {
+    return videos.filter((video) => video.tags.includes(genre));
+  };
+
+  return (
+    <>
+      <div className="app">
+        <div className="main-video">
+          <img
+            src={randomVideo(videos).thumbnail.url}
+            alt={randomVideo(videos).title}
+          />
+        </div>
+        <div className="video-feed">
+          <Section genre={"Family"} videos={filterVideos(videos, "family")} />
+          <Section genre={"Thriller"} videos={videos} />
+          <Section genre={"Classic"} videos={videos} />
+          <Section genre={"Pixar"} videos={videos} />
+          <Section genre={"Marvel"} videos={videos} />
+          <Section genre={"National Geographic"} videos={videos} />
+          <Section genre={"Disney"} videos={videos} />
+          <Section genre={"Star Wars"} videos={videos} />
+        </div>
+      </div>
+    </>
+  );
 }
